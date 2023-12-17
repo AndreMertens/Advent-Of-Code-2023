e16b971d6249cdd3fb4b93096c88151333435464
AndreMertens <54868177+AndreMertens@users.noreply.github.com>
2023-12-17 13:11:29 +0100

    solve part 2 of day 9

--- day_9/main.dart
+++ day_9/main.dart
@@ -44,46 +44,49 @@ void solveFirstPart(File file) {
 
 void solveSecondPart(File file) {
   final lines = file.readAsLinesSync();
-  int cardAmount = 0;
-  String pattern = '\\d+';
+  int historyValueSum = 0;
+  String pattern = '-*\\d+';
   RegExp regExp = RegExp(pattern);
-  final cardCopies = List.generate(lines.length, (index) => 0);
 
-  for (int lineIndex = 0; lineIndex < lines.length; lineIndex++) {
-    final line = lines[lineIndex];
-    List<String> cardElements = line.split(':');
-    cardElements = cardElements[1].split('|');
+  for (String line in lines) {
+    final sequence = regExp.allMatches(line).toList();
+    final List<int> intSequence =
+        sequence.map((e) => int.parse(e[0]!)).toList();
+    bool allvaluesNotZeros = true;
 
-    final winningNumberMatches = regExp.allMatches(cardElements[0]).toList();
-    final List<String> winningNumbers =
-        winningNumberMatches.map((match) => match[0]!).toList();
+    List<List<int>> history = [];
+    history.add(intSequence);
 
-    final numberMatches = regExp.allMatches(cardElements[1]).toList();
-    final List<String> numbers =
-        numberMatches.map((match) => match[0]!).toList();
-
-    int matchingNumbersAmount = 0;
-
-    for (String number in numbers) {
-      if (winningNumbers.contains(number)) {
-        matchingNumbersAmount += 1;
+    outerLoop:
+    while (allvaluesNotZeros) {
+      final List<int> diffSequence = [];
+      for (int index = 1; index < history.last.length; index++) {
+        diffSequence.add(history.last[index] - history.last[index - 1]);
       }
+
+      history.add(diffSequence);
+
+      for (int element in diffSequence) {
+        if (element != 0) {
+          continue outerLoop;
+        }
+      }
+      allvaluesNotZeros = false;
     }
 
-    for (int counter = 0; counter < matchingNumbersAmount; counter++) {
-      if (lineIndex + counter + 1 <= cardCopies.length) {
-        cardCopies[lineIndex + counter + 1] += 1 + cardCopies[lineIndex];
-      }
-    }
+    int historyValue = 0;
 
-    cardAmount += 1 + cardCopies[lineIndex];
+    for (int index = history.length - 1; index >= 0; index--) {
+      historyValue = history[index].first - historyValue;
+    }
+    historyValueSum += historyValue;
   }
-  print(cardAmount);
+  print(historyValueSum);
 }
 
 void main() {
   File file = File('input.txt');
 
   solveFirstPart(file);
-  //solveSecondPart(file);
+  solveSecondPart(file);
 }
